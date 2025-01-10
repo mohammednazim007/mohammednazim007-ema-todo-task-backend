2ed6566a536bcc409e7cc407233cec0b6a78923f
Mohammed Nazim <mohammednazim3629@gmail.com>
2025-01-10 22:24:18 +0600

    vercel file update

--- app/middleware.js
+++ app/middleware.js
@@ -1,14 +1,6 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const middleware = [
-  morgan("dev"),
-  cors({
-    origin: "http://localhost:3000", // Replace with your client's URL (for development)
-    methods: ["GET", "POST", "PUT", "DELETE"], // Allow these HTTP methods
-    allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
-  }),
-  express.json(),
-];
+const middleware = [morgan("dev"), cors(), express.json()];
 
 module.exports = middleware;
