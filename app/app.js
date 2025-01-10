32a4776723232c97508801ef37d3b185859156fb
Mohammed Nazim <mohammednazim3629@gmail.com>
2025-01-10 19:17:39 +0600

    vercel.json is modify

--- app/app.js
+++ app/app.js
@@ -1,34 +1,16 @@
-// require("dotenv").config("../.env");
-// const express = require("express");
-// const middleware = require("./middleware");
-// const router = require("./router");
-// const { notFoundHandler, errorHandler } = require("./error");
-
-// const app = express();
-
-// app.use(middleware);
-// app.use(router);
-
-// // global error handler
-// app.use(notFoundHandler);
-// app.use(errorHandler);
-
-// module.exports = app;
+require("dotenv").config("../.env");
 const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
+const middleware = require("./middleware");
+const router = require("./router");
+const { notFoundHandler, errorHandler } = require("./error");
 
-// CORS configuration
-const corsOptions = {
-  origin: "http://localhost:3000", // Allow requests only from your frontend
-  methods: "GET,POST,PUT,DELETE", // Allowed HTTP methods
-  allowedHeaders: "Content-Type,Authorization", // Allowed headers
-};
+const app = express();
 
-const middleware = [
-  morgan("dev"),
-  cors(corsOptions), // Apply CORS with options
-  express.json(), // Parse incoming JSON requests
-];
+app.use(middleware);
+app.use(router);
 
-module.exports = middleware;
+// global error handler
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+module.exports = app;
