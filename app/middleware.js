d5f1181e0d47955e2fa777fb8e5d50be3abf04e4
Mohammed Nazim <mohammednazim3629@gmail.com>
2025-01-10 19:50:53 +0600

    middleware file is changed

--- app/middleware.js
+++ app/middleware.js
@@ -1,6 +1,23 @@
-const express = require("express");
+// const express = require("express");
+// const cors = require("cors");
+// const morgan = require("morgan");
+// const middleware = [morgan("dev"), cors(), express.json()];
+
+// module.exports = middleware;const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const middleware = [morgan("dev"), cors(), express.json()];
+
+// CORS configuration
+const corsOptions = {
+  origin: "http://localhost:3000", // Your frontend URL during development
+  methods: "GET,POST,PUT,DELETE", // Allowed HTTP methods
+  allowedHeaders: "Content-Type,Authorization", // Allowed headers
+};
+
+const middleware = [
+  morgan("dev"),
+  cors(corsOptions), // Apply CORS with options
+  express.json(), // Parse incoming JSON requests
+];
 
 module.exports = middleware;
