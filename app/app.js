d6a6d00705e37fbea98563e3d26c16a53ae84e6d
Mohammed Nazim <mohammednazim3629@gmail.com>
2025-01-10 18:40:01 +0600

    cors is added

--- app/app.js
+++ app/app.js
@@ -3,11 +3,13 @@ const express = require("express");
 const middleware = require("./middleware");
 const router = require("./router");
 const { notFoundHandler, errorHandler } = require("./error");
+const cors = require("cors");
 
 const app = express();
 
 app.use(middleware);
 app.use(router);
+app.use(cors());
 
 // global error handler
 app.use(notFoundHandler);
