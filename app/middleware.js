e1384abbad2236799adb2376bf0d06d35d1f0ddc
Mohammed Nazim <mohammednazim3629@gmail.com>
2025-01-11 18:39:04 +0600

    errors

--- app/middleware.js
+++ app/middleware.js
@@ -4,7 +4,7 @@ const express = require("express");
 
 // Define the CORS options
 const corsOptions = {
-  origin: "*", // Allow all origins
+  origin: "http://localhost:3000",
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"], // Include required headers
   credentials: true,
