dc41f4c72d1e3a7aea27ff415fa102cff7e1437b
Mohammed Nazim <mohammednazim3629@gmail.com>
2025-01-11 18:32:01 +0600

    corsOptions is added

--- app/middleware.js
+++ app/middleware.js
@@ -1,14 +1,16 @@
-const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const express = require("express");
 
+// Define the CORS options
 const corsOptions = {
   origin: "http://localhost:3000",
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"], // Include required headers
   credentials: true,
 };
 
+// Middleware array
 const middleware = [cors(corsOptions), morgan("dev"), express.json()];
 
 module.exports = middleware;
