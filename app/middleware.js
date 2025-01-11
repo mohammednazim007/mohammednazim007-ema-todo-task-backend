436d69312d1039ef4a45e36044a99986ed7be435
Mohammed Nazim <mohammednazim3629@gmail.com>
2025-01-11 17:06:07 +0600

    add cors

--- app/middleware.js
+++ app/middleware.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const middleware = [cors(), morgan("dev"), express.json()];
+
+const corsOptions = {
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+};
+
+const middleware = [cors(corsOptions), morgan("dev"), express.json()];
 
 module.exports = middleware;
