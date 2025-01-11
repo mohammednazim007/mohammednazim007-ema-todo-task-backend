4d7cb7530c3e52a0ce3ce1cdd7d43365f1805df9
Mohammed Nazim <mohammednazim3629@gmail.com>
2025-01-11 20:24:47 +0600

    cors updated

--- app/middleware.js
+++ app/middleware.js
@@ -3,10 +3,16 @@ const cors = require("cors");
 const morgan = require("morgan");
 
 const corsOptions = {
-  origin: "http://localhost:3000",
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
+  origin: ["http://localhost:3000", "https://emaserver.vercel.app/api/v1"],
+  // methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+  // allowedHeaders: ["Content-Type", "Authorization"],
+  // credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allowed HTTP methods
+  allowedHeaders: [
+    "Content-Type",
+    "Authorization",
+    "Access-Control-Allow-Origin-Headers",
+  ],
 };
 
 const middleware = [cors(corsOptions), morgan("dev"), express.json()];
