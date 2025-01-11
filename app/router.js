b56dc0a380e577f8dacc8abdc6161944a59d106d
Mohammed Nazim <mohammednazim3629@gmail.com>
2025-01-11 16:36:32 +0600

    cors file remove

--- app/router.js
+++ app/router.js
@@ -2,11 +2,7 @@ const router = require("express").Router();
 const userRouter = require("../controller/user/user.route");
 const taskRouter = require("../controller/task-controller/task.route");
 const { getCategories } = require("../controller/user/user.controller");
-router.use(
-  cors({
-    origin: "*",
-  })
-);
+
 router.use("/api/v1/tasks", userRouter);
 router.use("/api/v1/users", taskRouter);
 
